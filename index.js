a71e4d5a089cbf0a7736398341aec3c761f266be
fenrirqutrub <fenrirqutrub@gmail.com>
2025-11-08 22:06:03 +0600

    add cors

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ const upload = multer({
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://fenrirqutrub.vercel.app" || "http://localhost:5173",
     credentials: true,
   })
 );
